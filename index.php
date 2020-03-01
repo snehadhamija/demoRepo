2c2575475cdffbbbd5f8d27ade1abcbab0f8548f
snehadhamija <61445355+snehadhamija@users.noreply.github.com>
2020-03-01 15:25:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
+<?php include("api.php") ;
+	echo "testing";
+	$response = api::call();
+?> 
 <!DOCTYPE HTML>
 <html>
 <?php include("HEAD.PHP"); ?>
